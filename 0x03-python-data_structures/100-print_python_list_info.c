d8ac621790c1e35878a991db29ac04bc5d8cddd2
Taibi EL Yakouti <111664715+xDweeb@users.noreply.github.com>
2024-07-16 00:04:39 +0000

    python

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,26 +1,17 @@
-#include "/usr/include/python3.4/Python.h"
-#include "/usr/include/python3.4/listobject.h"
-#include "/usr/include/python3.4/object.h"
+#include <stdlib.h>
 #include <stdio.h>
-
+#include <Python.h>
 /**
- * print_python_list_info - prints some basic info about Python lists
+ * print_python_list_info -  function that prints some basic
+ *							info about Python lists
  * @p: python list
- * Return: nothing
  */
-
 void print_python_list_info(PyObject *p)
 {
-	PyListObject *python_list = (PyListObject *) p;
-	int i, list_length = Py_SIZE(p);
-	PyObject *element;
+	int elem;
 
-	printf("[*] Size of the Python List = %d\n", list_length);
-	printf("[*] Allocated = %d\n", (int) python_list->allocated);
-
-	for (i = 0; i < list_length; i++)
-	{
-		element = PyList_GET_ITEM(p, i);
-		printf("Element %d: %s\n", i, element->ob_type->tp_name);
-	}
+	printf("[*] Size of the Python List = %lu\n", Py_SIZE(p));
+	printf("[*] Allocated = %lu\n", ((PyListObject *)p)->allocated);
+	for (elem = 0; elem < Py_SIZE(p); elem++)
+		printf("Element %d: %s\n", elem, Py_TYPE(PyList_GetItem(p, elem))->tp_name);
 }
